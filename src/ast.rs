83deadccbf8be2903f44dfb96644007634782ca0
Taiki Harada <haradataiki19990927@gmail.com>
2021-04-29 15:04:36 +0900

    テストコードの修正

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,10 @@
+#[derive(Debug, PartialEq)]
 pub enum Expr {
     Number(i32),
     Op(Box<Expr>, Opcode, Box<Expr>),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Opcode {
     Mul,
     Div,
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #[macro_use]
 extern crate lalrpop_util;
 
-lalrpop_mod!(pub ast_parser);
 pub mod ast;
+#[allow(unused_imports)]
+use ast::*;
+
+// cargo run build scriptsで.lalrpopファイルを読み込みパーサーを生成する
+lalrpop_mod!(pub ast_parser);
 
 fn main() {
     println!("Hello, world!");
@@ -11,5 +15,16 @@ fn main() {
 #[test]
 fn calculator4() {
     let expr = ast_parser::ExprParser::new().parse("22 * 44 + 66").unwrap();
-    assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
+    assert_eq!(
+        Box::new(Expr::Op(
+            Box::new(Expr::Op(
+                Box::new(Expr::Number(22)),
+                Opcode::Mul,
+                Box::new(Expr::Number(44)),
+            )),
+            Opcode::Add,
+            Box::new(Expr::Number(66)),
+        )),
+        expr
+    );
 }
